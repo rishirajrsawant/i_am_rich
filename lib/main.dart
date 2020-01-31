2baaafd2d085fb33cfdaf4533b87f7918fdae37e
rishirajrsawant <rishiraj.sawant97@gmail.com>
2020-01-31 19:38:54 +0000

    v1.0 initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+//main function to start a flutter app
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+      ),
+    ),
+  );
+}
