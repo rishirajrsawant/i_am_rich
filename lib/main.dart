fd03d7c129f2258f8668c0bad5494c8afd7c426c
rishirajrsawant <rishiraj.sawant97@gmail.com>
2020-02-01 01:21:18 +0000

    v1.1 first commit -added an image from a URL using NetworkImage widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,11 @@ void main() {
           backgroundColor: Colors.blueGrey[900],
         ),
         backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+              image: NetworkImage(
+                  'https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')),
+        ),
       ),
     ),
   );
